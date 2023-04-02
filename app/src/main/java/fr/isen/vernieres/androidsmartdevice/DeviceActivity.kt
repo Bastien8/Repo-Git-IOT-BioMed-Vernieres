1a044bb65167cffa055d6daa84ae52fdb4a786c3
Bastien8 <bastien.vernieres@isen.yncrea.fr>
2023-04-02 15:51:05 +0200

    commit 4

--- app/src/main/java/fr/isen/vernieres/androidsmartdevice/DeviceActivity.kt
+++ app/src/main/java/fr/isen/vernieres/androidsmartdevice/DeviceActivity.kt
@@ -1,11 +1,46 @@
 package fr.isen.vernieres.androidsmartdevice
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothProfile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.view.isVisible
+import fr.isen.vernieres.androidsmartdevice.databinding.ActivityDeviceBinding
 
+@SuppressLint("MissingPermission")
 class DeviceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDeviceBinding
+    private var bluetoothGatt: BluetoothGatt? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device)
+        binding = ActivityDeviceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bluetoothDevice: BluetoothDevice? = intent.getParcelableExtra("device")
+        val bluetoothGatt: bluetoothDevice?.connectGatt(this, false, bluetoothGattCallback)
+        bluetoothGatt?.connect()
+    }
+
+
+    override fun onStop(){
+        super.onStop()
+        bluetoothGatt?.close()
+    }
+
+    private val bluetoothGattCallback = object : BluetoothGattCallback(){
+        override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                displayContentConnected()
+            }
+        }
+    }
+    private fun displayContentConnected(){
+        binding.Title.text = getString(R.string.Title)
+        binding.progressBar2.isVisible = false
+        binding.imageView2.isVisible =  true
     }
 }
